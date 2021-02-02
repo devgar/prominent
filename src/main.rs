c88f98d2621fab6eb4cbe718eb8153d906bd8280
Edgar Albalate <dev.gardo@gmail.com>
2021-02-02 12:09:01 +0100

    Version with custom config

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use std::env;
 #[macro_use] extern crate rocket;
+use rocket::config::{Config, Environment};
 
 #[get("/")]
 fn index() -> &'static str {
   "Hello, world!"
 }
 
-fn main() { 
-  rocket::ignite().mount("/", routes![index]).launch();
+fn parse_port(v: String) ->  Result<u16, env::VarError> {
+  Ok(v.parse::<u16>().expect("PORT must be a valid u16 number"))
+}
+
+fn main() {
+  let port = env::var("PORT").and_then(parse_port).unwrap_or(8000);
+
+  let config = Config::build(Environment::Staging)
+    .port(port).finalize().expect("Not a valid config");
+
+  rocket::custom(config).mount("/", routes![index]).launch();
 }
